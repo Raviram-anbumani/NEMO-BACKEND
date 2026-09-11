62f8e5c58b1b4cc6da70f2bd54d889b350f50efc
Saravanan-2506 <rsaravanan2506@gmail.com>
2026-09-11 16:53:59 +0530

    Preparing for railway

--- src/main/java/com/nemo/backend/config/CorsConfig.java
+++ src/main/java/com/nemo/backend/config/CorsConfig.java
@@ -10,10 +10,12 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/api/**")
                         .allowedOrigins(
                                 "http://127.0.0.1:5500",
