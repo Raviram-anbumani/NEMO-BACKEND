6bd8cccae7c7df069ae56a42d6dc323e7bca2bbf
Saravanan-2506 <rsaravanan2506@gmail.com>
2026-09-11 17:46:10 +0530

    Cors config with vercel

--- src/main/java/com/nemo/backend/config/CorsConfig.java
+++ src/main/java/com/nemo/backend/config/CorsConfig.java
@@ -10,16 +10,15 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry.addMapping("/api/**")
                         .allowedOrigins(
                                 "http://127.0.0.1:5500",
-                                "http://localhost:5500"
+                                "http://localhost:5500",
+                                "https://nemo-new-frontend.vercel.app"
                         )
                         .allowedMethods(
                                 "GET",
